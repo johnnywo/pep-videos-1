3b9a1046a97422734cc7ba643a103b35cd3730ce
johnnywo <office@zeero.at>
2021-03-23 08:48:46 +0100

    neues video

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,8 +10,7 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     <!-- Styles -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <style>
         body {
             color: rgba(20, 102, 122, 0.55);
@@ -28,88 +28,91 @@
         }
     </style>
 </head>
+
 <body>
-<div class="row" style="padding-top: 50px;">
-    <div class="col-sm-6 col-sm-offset-3">
+    <div class="row" style="padding-top: 50px;">
+        <div class="col-sm-6 col-sm-offset-3">
 
-        <h3>mit deutscher Übersetzung</h3>
-        <hr>
-        <ul>
-            <li><a href="intro-to-pep.php">Einführung in das Friedensbildungsprogramm (Dauer 6:00)</a></li>
-            <li><a href="university-texas.php">University of Texas, San Antonio (Dauer 22:55)</a></li>
-            <li><a href="choice-dvd.php">Prem Rawat - Choice (Dauer 27:07)</a></li>
-            <li><a href="best-of-2012.php">Best of 2012, Prem Rawat 1/2 (Dauer 56:52)</a></li>
-            <li><a href="best-of-2012-2.php">Best of 2012, Prem Rawat 2/2 (Dauer 1:04:05)</a></li>
-            <li><a href="pr-sanantonio-events.php">Prem Rawat - San Antonio Events 2012 (Dauer 22:55)</a></li>
-            <li><a href="pr-wembley-london.php">Prem Rawat - Wembley, London (Dauer 1:18:23)</a></li>
-            <li><a href="pr-santiago-chile.php">Prem Rawat - Santiago, Chile (Dauer 1:03:47)</a></li>
-            <li><a href="pr-salvator-brazil.php">Prem Rawat - Salvator, Brazil (Dauer 1:07:52)</a></li>
-            <li><a href="pr-pasadena-california.php">Prem Rawat - Pasadena, California (Dauer 1:04:53)</a></li>
-            <li><a href="pr-miami-florida.php">Prem Rawat - Miami, Florida (Dauer 1:06:49)</a></li>
-            <li><a href="pr-lima-peru.php">Prem Rawat - Lima, Peru (Dauer 1:05:11)</a></li>
-        </ul>
-        <br>
-        <h3>mit deutschen Untertiteln</h3>
-        <hr>
-        <ul>
-            <li><a href="light-up-the-world.php">Light up the World (Dauer 4:15)</a></li>
-            <li><a href="frieden-fuehlen.php">Einführung in den Friedenskurs (Dauer 5:59)</a></li>
-            <li><a href="prem-rawat-sein-anliegen.php">Prem Rawat - Sein Anliegen (Dauer 4:13)</a></li>
-            <li><a href="suche-nach-der-eigenen-natur.php">Suche nach der eigenen Natur (Dauer 27:55)</a></li>
-            <li><a href="wuerde-frieden-wohlstand.php">Würde, Frieden, Wohlstand (Dauer 11:02)</a></li>
-            <li><a href="choice-entscheidungsfreiheit.php">Choice, Entscheidungsfreiheit (Dauer 27:07)</a></li>
-            <li><a href="frieden-ist-unumgaenglich.php">Frieden ist Unumgänglich (Dauer 5:40)</a></li>
-            <li><a href="nordic-peace-conference.php">Nordic Peace Conference (Dauer 13:59)</a></li>
-            <li><a href="gespraech-mit-lumka-ngxoli.php">Gespräch mit Lumka Ngxoli (Dauer 24:45)</a></li>
-            <li><a href="italienischer-senat-rom.php">Italienischer Senat, Rom (Dauer 7:17)</a></li>
-            <li><a href="die-kraft-zur-veraenderung.php">Die Kraft zur Veränderung (Dauer 16:46)</a></li>
-            <li><a href="geschichten-zum-frieden.php">Geschichten zum Frieden (Dauer 14:48)</a></li>
-            <li><a href="tprf-peace-edu-program.php">TPRF's Peace Education Program (PEP) (Dauer 3:32)</a></li>
-            <li><a href="what-the-heart-is.php">What the heart is (Dauer 10:23)</a></li>
-            <li><a href="peace-on-the-outside.php">Peace on the Outside (Dauer 16:48)</a></li>
-        </ul>
-        <br>
-        <h3>Englisch</h3>
-        <hr>
-        <ul>
-            <li><a href="bbc-news.php">15. Juni 2018, BBC und Londoner Bürgermeister propagieren das Friedensbildungsprogramm für alle Schüler ab 14 Jahren (Dauer 47s)</a></li>
-            <li><a href="inside-peace.php">Inside Peace (Dauer 1:33:27)</a></li>
-            <li><a href="waf-award-2014-london.php">WAF Award, London 2014 (Dauer 23:53)</a></li>
-            <li><a href="prem-rawats-patron-speech.php">Prem Rawats Patron Speech (Dauer 5:03)</a></li>
-            <li><a href="prem-rawat-house-of-parliament.php">Prem Rawat House of Parliament, London (Dauer 43:02)</a></li>
-            <li><a href="friedensstifter.php">Friedensstifter - Jermey Gilley im Gespräch mit Prem Rawat (Dauer 9:23)</a></li>
-            <li><a href="2014-highlights-part-one.php">2014 Highlights Part One (Dauer 58:09)</a></li>
-            <li><a href="2014-highlights-part-two.php">2014 Highlights Part Two (Dauer 56:00)</a></li>
-            <li><a href="2014-highlights-part-three.php">2014 Highlights Part Three (Dauer 1:00:54)</a></li>
-            <li><a href="einfachheit.php">Einfachheit (Dauer 23:48)</a></li>
-            <li><a href="zonderwater-kurzversion.php">Zonderwater Kurzversion (Dauer 4:34)</a></li>
-            <li><a href="prem-rawat-san-antonio-2012.php">Prem Rawat at San Antonio Events 2012</a></li>
-            <li><a href="the-nation-interview.php">The Nation - Interview with Prem Rawat (Dauer 56:09) </a></li>
-            <li><a href="mr-gee-in-conversation-with-pr.php">Mr. Gee in Conversation with Prem Rawat (Dauer 23:48)</a></li>
-            <li><a href="appreciation.php">Appreciation: Prem Rawat (Dauer 28:03)</a></li>
-            <li><a href="a-lifetime.php">A Lifetime: Prem Rawat (Dauer 24:26)</a></li>
-            <li><a href="a-veteran-speaks-on-peace.php">A Veteran speaks on Peace (Dauer: 4:29)</a></li>
-            <li><a href="your-secret-weapon.php">Your secret weapon (Dauer: 1:24)</a></li>
-            <li><a href="inside-peace-screening.php">Inside Peace Screening - Celebrate Life Events (Dauer 7:33)</a></li>
-            <li><a href="pr-european-parliament.php">Prem Rawat at the European Parliament, Brussels, Belgium 2010 (Dauer 24:24)</a></li>
-            <li><a href="http://www.tprf.org/category/news/">TPRF.org NEWS</a></li>
-            <li><a href="jeremy-gilley-im-gespraech-mit-prem-rawat.php">Friedensstifter - Jeremy Gilley im Gespräch mit Prem Rawat (Dauer 28:12)</a></li>
-            <li><a href="peace-education-in-columbia.php">Peace Education in Columbia (Dauer 10:37)</a></li>
-            <li><a href="sharing-a-dream.php">Sharing a Dream (Dauer 9:57)</a></li>
-            <li><a href="peace-education-and-rehabilitation.php">Peace Education and Rehabilitation (Dauer 14:00)</a></li>
-            <li><a href="peace-the-most-practical-tool.php">Peace: the most practical tool (Dauer 22:39)</a></li>
-            <li><a href="change-through-peace-education.php">Change through peace education (Dauer 07:16)</a></li>
-        </ul>
-        <br>
-        <h3>Diashow mit Musik</h3>
-        <hr>
-        <ul>
-            <li><a href="womens-prison-argentina.php">Prem Rawat in Women's Prison in Argentina (Dauer 4:55)</a></li>
-        </ul>
-        <br>
-        <br>
+            <h3>mit deutscher Übersetzung</h3>
+            <hr>
+            <ul>
+                <li><a href="intro-to-pep.php">Einführung in das Friedensbildungsprogramm (Dauer 6:00)</a></li>
+                <li><a href="university-texas.php">University of Texas, San Antonio (Dauer 22:55)</a></li>
+                <li><a href="choice-dvd.php">Prem Rawat - Choice (Dauer 27:07)</a></li>
+                <li><a href="best-of-2012.php">Best of 2012, Prem Rawat 1/2 (Dauer 56:52)</a></li>
+                <li><a href="best-of-2012-2.php">Best of 2012, Prem Rawat 2/2 (Dauer 1:04:05)</a></li>
+                <li><a href="pr-sanantonio-events.php">Prem Rawat - San Antonio Events 2012 (Dauer 22:55)</a></li>
+                <li><a href="pr-wembley-london.php">Prem Rawat - Wembley, London (Dauer 1:18:23)</a></li>
+                <li><a href="pr-santiago-chile.php">Prem Rawat - Santiago, Chile (Dauer 1:03:47)</a></li>
+                <li><a href="pr-salvator-brazil.php">Prem Rawat - Salvator, Brazil (Dauer 1:07:52)</a></li>
+                <li><a href="pr-pasadena-california.php">Prem Rawat - Pasadena, California (Dauer 1:04:53)</a></li>
+                <li><a href="pr-miami-florida.php">Prem Rawat - Miami, Florida (Dauer 1:06:49)</a></li>
+                <li><a href="pr-lima-peru.php">Prem Rawat - Lima, Peru (Dauer 1:05:11)</a></li>
+            </ul>
+            <br>
+            <h3>mit deutschen Untertiteln</h3>
+            <hr>
+            <ul>
+                <li><a href="einfuehrung-pep-kurs.php"></a>Einführung PEP Kurs (Dauer 9:42)</li>
+                <li><a href="light-up-the-world.php">Light up the World (Dauer 4:15)</a></li>
+                <li><a href="frieden-fuehlen.php">Einführung in den Friedenskurs (Dauer 5:59)</a></li>
+                <li><a href="prem-rawat-sein-anliegen.php">Prem Rawat - Sein Anliegen (Dauer 4:13)</a></li>
+                <li><a href="suche-nach-der-eigenen-natur.php">Suche nach der eigenen Natur (Dauer 27:55)</a></li>
+                <li><a href="wuerde-frieden-wohlstand.php">Würde, Frieden, Wohlstand (Dauer 11:02)</a></li>
+                <li><a href="choice-entscheidungsfreiheit.php">Choice, Entscheidungsfreiheit (Dauer 27:07)</a></li>
+                <li><a href="frieden-ist-unumgaenglich.php">Frieden ist Unumgänglich (Dauer 5:40)</a></li>
+                <li><a href="nordic-peace-conference.php">Nordic Peace Conference (Dauer 13:59)</a></li>
+                <li><a href="gespraech-mit-lumka-ngxoli.php">Gespräch mit Lumka Ngxoli (Dauer 24:45)</a></li>
+                <li><a href="italienischer-senat-rom.php">Italienischer Senat, Rom (Dauer 7:17)</a></li>
+                <li><a href="die-kraft-zur-veraenderung.php">Die Kraft zur Veränderung (Dauer 16:46)</a></li>
+                <li><a href="geschichten-zum-frieden.php">Geschichten zum Frieden (Dauer 14:48)</a></li>
+                <li><a href="tprf-peace-edu-program.php">TPRF's Peace Education Program (PEP) (Dauer 3:32)</a></li>
+                <li><a href="what-the-heart-is.php">What the heart is (Dauer 10:23)</a></li>
+                <li><a href="peace-on-the-outside.php">Peace on the Outside (Dauer 16:48)</a></li>
+            </ul>
+            <br>
+            <h3>Englisch</h3>
+            <hr>
+            <ul>
+                <li><a href="bbc-news.php">15. Juni 2018, BBC und Londoner Bürgermeister propagieren das Friedensbildungsprogramm für alle Schüler ab 14 Jahren (Dauer 47s)</a></li>
+                <li><a href="inside-peace.php">Inside Peace (Dauer 1:33:27)</a></li>
+                <li><a href="waf-award-2014-london.php">WAF Award, London 2014 (Dauer 23:53)</a></li>
+                <li><a href="prem-rawats-patron-speech.php">Prem Rawats Patron Speech (Dauer 5:03)</a></li>
+                <li><a href="prem-rawat-house-of-parliament.php">Prem Rawat House of Parliament, London (Dauer 43:02)</a></li>
+                <li><a href="friedensstifter.php">Friedensstifter - Jermey Gilley im Gespräch mit Prem Rawat (Dauer 9:23)</a></li>
+                <li><a href="2014-highlights-part-one.php">2014 Highlights Part One (Dauer 58:09)</a></li>
+                <li><a href="2014-highlights-part-two.php">2014 Highlights Part Two (Dauer 56:00)</a></li>
+                <li><a href="2014-highlights-part-three.php">2014 Highlights Part Three (Dauer 1:00:54)</a></li>
+                <li><a href="einfachheit.php">Einfachheit (Dauer 23:48)</a></li>
+                <li><a href="zonderwater-kurzversion.php">Zonderwater Kurzversion (Dauer 4:34)</a></li>
+                <li><a href="prem-rawat-san-antonio-2012.php">Prem Rawat at San Antonio Events 2012</a></li>
+                <li><a href="the-nation-interview.php">The Nation - Interview with Prem Rawat (Dauer 56:09) </a></li>
+                <li><a href="mr-gee-in-conversation-with-pr.php">Mr. Gee in Conversation with Prem Rawat (Dauer 23:48)</a></li>
+                <li><a href="appreciation.php">Appreciation: Prem Rawat (Dauer 28:03)</a></li>
+                <li><a href="a-lifetime.php">A Lifetime: Prem Rawat (Dauer 24:26)</a></li>
+                <li><a href="a-veteran-speaks-on-peace.php">A Veteran speaks on Peace (Dauer: 4:29)</a></li>
+                <li><a href="your-secret-weapon.php">Your secret weapon (Dauer: 1:24)</a></li>
+                <li><a href="inside-peace-screening.php">Inside Peace Screening - Celebrate Life Events (Dauer 7:33)</a></li>
+                <li><a href="pr-european-parliament.php">Prem Rawat at the European Parliament, Brussels, Belgium 2010 (Dauer 24:24)</a></li>
+                <li><a href="http://www.tprf.org/category/news/">TPRF.org NEWS</a></li>
+                <li><a href="jeremy-gilley-im-gespraech-mit-prem-rawat.php">Friedensstifter - Jeremy Gilley im Gespräch mit Prem Rawat (Dauer 28:12)</a></li>
+                <li><a href="peace-education-in-columbia.php">Peace Education in Columbia (Dauer 10:37)</a></li>
+                <li><a href="sharing-a-dream.php">Sharing a Dream (Dauer 9:57)</a></li>
+                <li><a href="peace-education-and-rehabilitation.php">Peace Education and Rehabilitation (Dauer 14:00)</a></li>
+                <li><a href="peace-the-most-practical-tool.php">Peace: the most practical tool (Dauer 22:39)</a></li>
+                <li><a href="change-through-peace-education.php">Change through peace education (Dauer 07:16)</a></li>
+            </ul>
+            <br>
+            <h3>Diashow mit Musik</h3>
+            <hr>
+            <ul>
+                <li><a href="womens-prison-argentina.php">Prem Rawat in Women's Prison in Argentina (Dauer 4:55)</a></li>
+            </ul>
+            <br>
+            <br>
 
+        </div>
     </div>
-</div>
 </body>
+
 </html>
